3cd7c0881f39d921c457ed27e8ada7264951bf33
Machi <Angelo.kamachi03@gmail.com>
2025-05-31 21:27:44 +0800

    redirect test

---
+++ src/php/test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Kamachi Dev</title>
+        <?php include 'templates/master.html'; ?>
+    </head>
+    <?php include 'templates/header.html'; ?>
+    <body>
+        WOW!!!!!!!!!!!!!!!!!!!!
+    </body>
+    <?php include 'templates/footer.html'; ?>
+</html>
+
+<?php
+?>O
